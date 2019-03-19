da6aecbef9432fde1f4f62e569e0aa5422e738bb
KaiHenssen <kai.henssen@googlemail.com>
2019-03-19 15:51:50 +0000

    https://github.com/KaiHenssen/Camera.git

--- src/main/java/com/develogical/camera/Camera.java
+++ src/main/java/com/develogical/camera/Camera.java
@@ -2,16 +2,23 @@ package com.develogical.camera;
 
 public class Camera {
 
+    Sensor sensor = null;
+
+    public Camera(Sensor sensor) {
+        this.sensor = sensor;
+
+    }
+
     public void pressShutter() {
         // not implemented
     }
 
     public void powerOn() {
-        // not implemented
+        sensor.powerUp();
     }
 
     public void powerOff() {
-       // not implemented
+        sensor.powerDown();
+
     }
 }
-
--- src/test/java/com/develogical/camera/CameraTest.java
+++ src/test/java/com/develogical/camera/CameraTest.java
@@ -8,6 +8,22 @@ import static org.mockito.Mockito.*;
 public class CameraTest {
     @Test
     public void switchingTheCameraOnPowersUpTheSensor() {
-        // write your test here
+        Sensor sensor = mock(Sensor.class);
+        Camera camera = new Camera(sensor);
+
+        camera.powerOn();
+
+        verify(sensor).powerUp();
     }
+
+    @Test
+    public void switchingTheCameraOffPowersDownTheSensor() {
+        Sensor sensor = mock(Sensor.class);
+        Camera camera = new Camera(sensor);
+
+        camera.powerOff();
+
+        verify(sensor).powerDown();
+    }
+
 }
