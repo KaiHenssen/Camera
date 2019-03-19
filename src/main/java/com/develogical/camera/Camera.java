5efcb8c6a2e584fd4622fb00e527bb9863048fed
KaiHenssen <kai.henssen@googlemail.com>
2019-03-19 16:48:46 +0000

    Working with Inam
    - changes so far: Up to step four
    
    - lessons learned:
    - - use private final when possible
    - - mock syntax
    - - use of "any()" and "eq()"" (matcher)

--- src/main/java/com/develogical/camera/Camera.java
+++ src/main/java/com/develogical/camera/Camera.java
@@ -2,22 +2,31 @@ package com.develogical.camera;
 
 public class Camera {
 
-    Sensor sensor = null;
+    private final Sensor sensor;
+    private final MemoryCard memorycard;
+    private final WriteCompleteListener writeCompleteListener;
+    private boolean ison = false;
 
-    public Camera(Sensor sensor) {
+    public Camera(Sensor sensor, MemoryCard memorycard, WriteCompleteListener writeCompleteListener) {
         this.sensor = sensor;
-
+        this.memorycard = memorycard;
+        this.writeCompleteListener = writeCompleteListener;
     }
 
     public void pressShutter() {
-        // not implemented
+        if (ison) {
+            //WriteCompleteListener writeCompleteListener = new WriteCompleteListener();
+            byte[] dateToWriteToMemoryCard = sensor.readData();
+            memorycard.write(dateToWriteToMemoryCard, writeCompleteListener);
+        }
     }
-
     public void powerOn() {
+        ison=true;
         sensor.powerUp();
     }
 
     public void powerOff() {
+        ison=false;
         sensor.powerDown();
 
     }
--- src/test/java/com/develogical/camera/CameraTest.java
+++ src/test/java/com/develogical/camera/CameraTest.java
@@ -1,7 +1,6 @@
 package com.develogical.camera;
 
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
 
 import static org.mockito.Mockito.*;
 
@@ -9,7 +8,7 @@ public class CameraTest {
     @Test
     public void switchingTheCameraOnPowersUpTheSensor() {
         Sensor sensor = mock(Sensor.class);
-        Camera camera = new Camera(sensor);
+        Camera camera = new Camera(sensor, null, null);
 
         camera.powerOn();
 
@@ -19,11 +18,37 @@ public class CameraTest {
     @Test
     public void switchingTheCameraOffPowersDownTheSensor() {
         Sensor sensor = mock(Sensor.class);
-        Camera camera = new Camera(sensor);
+        Camera camera = new Camera(sensor, null, null);
 
         camera.powerOff();
 
         verify(sensor).powerDown();
     }
 
+    @Test
+    public void pressingShutterWhilePowerOn() {
+        Sensor sensor = mock(Sensor.class);
+        MemoryCard memorycard = mock(MemoryCard.class);
+        WriteCompleteListener writeCompleteListener = mock(WriteCompleteListener.class);
+
+        Camera camera = new Camera(sensor, memorycard, writeCompleteListener);
+
+        camera.powerOn();
+        camera.pressShutter();
+
+        verify(memorycard).write(eq(sensor.readData()), any());
+    }
+
+    @Test
+    public void pressingShutterWhilePowerOff() {
+        Sensor sensor = mock(Sensor.class);
+        MemoryCard memorycard = mock(MemoryCard.class);
+        WriteCompleteListener writeCompleteListener = mock(WriteCompleteListener.class);
+
+        Camera camera = new Camera(sensor, memorycard, writeCompleteListener);
+
+        verifyNoMoreInteractions(sensor,memorycard);
+        camera.pressShutter();
+
+    }
 }
